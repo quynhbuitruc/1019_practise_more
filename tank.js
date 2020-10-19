b9d9e0d325e71333e37ea0e14caec5b3d6820715
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-20 00:28:47 +0900

    sửa function moveRight

--- tank.js
+++ tank.js
@@ -1,39 +1,15 @@
 function moveRight() {
     let x = parseInt(document.getElementById("tank").style.left)
     console.log(x)
-    if (x < 171) {
+    if (x == 95) {
+        document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("rainBow").style.visibility = "visible"
+        document.getElementById("tank").style.left = x + 19 + "px"
+    } else if (x < 171) {
+        document.getElementById("rainBow").style.visibility = "hidden"
+        document.getElementById("tank").style.visibility = "visible"
         document.getElementById("tank").style.left = x + 19 + "px"
     } else {
         document.getElementById("tank").style.left = "0px"
-    }
-}
-
-function moveLeft() {
-    let x = parseInt(document.getElementById("tank").style.left)
-    console.log(x)
-    if (x > 19) {
-        document.getElementById("tank").style.left = x - 19 + "px"
-    } else {
-        document.getElementById("tank").style.left = "171px"
-    }
-}
-
-function moveDown() {
-    let x = parseInt(document.getElementById("tank").style.top)
-    console.log(x)
-    if (x < 171) {
-        document.getElementById("tank").style.top = x + 19 + "px"
-    } else {
-        document.getElementById("tank").style.top = "0px"
-    }
-}
-
-function moveUp() {
-    let x = parseInt(document.getElementById("tank").style.top)
-    console.log(x)
-    if (x > 0) {
-        document.getElementById("tank").style.top = x - 19 + "px"
-    } else {
-        document.getElementById("tank").style.top = "171px"
     }
 }
