4134375c91614f1f29140de0effb2c7e9ab1b878
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-21 11:38:04 +0900

    tạo function moveUp

--- tank.js
+++ tank.js
@@ -29,3 +29,19 @@ function moveLeft() {
         document.getElementById("tank").style.left = "171px"
     }
 }
+
+function moveUp() {
+    let y = parseInt(document.getElementById("tank").style.top)
+    console.log(y)
+    if (y == 95) {
+        document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("rainBow").style.visibility = "visible"
+        document.getElementById("tank").style.top = y - 19 + "px"
+    } else if (y > 0) {
+        document.getElementById("rainBow").style.visibility = "hidden"
+        document.getElementById("tank").style.visibility = "visible"
+        document.getElementById("tank").style.top = y - 19 + "px"
+    } else {
+        document.getElementById("tank").style.top = "171px"
+    }
+}
