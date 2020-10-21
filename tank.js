41c85dd5fed9a6a0bcf95764fabce47161ecff61
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-21 18:39:53 +0900

    chỉnh tọa độ

--- tank.js
+++ tank.js
@@ -1,11 +1,9 @@
 function moveRight() {
     let x = parseInt(document.getElementById("tank").style.left)
     let y = parseInt(document.getElementById("tank").style.top)
-    let Xtreasure = parseInt(document.getElementById("treasure").style.left)
-    let Ytreasure = parseInt(document.getElementById("treasure").style.top)
-    if (x < 171) {
-        if (x == Xtreasure) {
-            if (y == Ytreasure) {
+    if (x < 190) {
+        if (x == 76) {
+            if (y == 95) {
                 document.getElementById("tank").style.visibility = "hidden"
                 document.getElementById("treasure").style.visibility = "hidden"
                 document.getElementById("rainBow").style.visibility = "visible"
@@ -30,11 +28,9 @@ function moveRight() {
 function moveLeft() {
     let x = parseInt(document.getElementById("tank").style.left)
     let y = parseInt(document.getElementById("tank").style.top)
-    let Xtreasure = parseInt(document.getElementById("treasure").style.left)
-    let Ytreasure = parseInt(document.getElementById("treasure").style.top)
     if (x > 0) {
-        if (x == Xtreasure) {
-            if (y == Ytreasure) {
+        if (x == 114) {
+            if (y == 95) {
                 document.getElementById("tank").style.visibility = "hidden"
                 document.getElementById("treasure").style.visibility = "hidden"
                 document.getElementById("rainBow").style.visibility = "visible"
@@ -52,18 +48,16 @@ function moveLeft() {
             document.getElementById("tank").style.left = x - 19 + "px"
         }
     } else {
-        document.getElementById("tank").style.left = "171px"
+        document.getElementById("tank").style.left = "190px"
     }
 }
 
 function moveUp() {
     let x = parseInt(document.getElementById("tank").style.left)
     let y = parseInt(document.getElementById("tank").style.top)
-    let Xtreasure = parseInt(document.getElementById("treasure").style.left)
-    let Ytreasure = parseInt(document.getElementById("treasure").style.top)
     if (y > 0) {
-        if (y == Ytreasure + 19) {
-            if (x == Xtreasure) {
+        if (y == 114) {
+            if (x == 95) {
                 document.getElementById("tank").style.visibility = "hidden"
                 document.getElementById("treasure").style.visibility = "hidden"
                 document.getElementById("rainBow").style.visibility = "visible"
@@ -81,6 +75,33 @@ function moveUp() {
             document.getElementById("tank").style.top = y - 19 + "px"
         }
     } else {
-        document.getElementById("tank").style.top = "171px"
+        document.getElementById("tank").style.top = "190px"
+    }
+}
+
+function moveDown() {
+    let x = parseInt(document.getElementById("tank").style.left)
+    let y = parseInt(document.getElementById("tank").style.top)
+    if (y < 190) {
+        if (y == 76) {
+            if (x == 95) {
+                document.getElementById("tank").style.visibility = "hidden"
+                document.getElementById("treasure").style.visibility = "hidden"
+                document.getElementById("rainBow").style.visibility = "visible"
+                document.getElementById("tank").style.top = y + 19 + "px"
+            } else {
+                document.getElementById("treasure").style.visibility = "visible"
+                document.getElementById("tank").style.visibility = "visible"
+                document.getElementById("rainBow").style.visibility = "hidden"
+                document.getElementById("tank").style.top = y + 19 + "px"
+            }
+        } else {
+            document.getElementById("treasure").style.visibility = "visible"
+            document.getElementById("tank").style.visibility = "visible"
+            document.getElementById("rainBow").style.visibility = "hidden"
+            document.getElementById("tank").style.top = y + 19 + "px"
+        }
+    } else {
+        document.getElementById("tank").style.top = "0px"
     }
 }
