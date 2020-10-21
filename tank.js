56481acccc71abaf84f5b3f4de06a29166a92e8e
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-21 11:32:47 +0900

    them function moveLeft

--- tank.js
+++ tank.js
@@ -13,3 +13,19 @@ function moveRight() {
         document.getElementById("tank").style.left = "0px"
     }
 }
+
+function moveLeft() {
+    let x = parseInt(document.getElementById("tank").style.left)
+    console.log(x)
+    if (x == 95) {
+        document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("rainBow").style.visibility = "visible"
+        document.getElementById("tank").style.left = x - 19 + "px"
+    } else if (x > 0) {
+        document.getElementById("rainBow").style.visibility = "hidden"
+        document.getElementById("tank").style.visibility = "visible"
+        document.getElementById("tank").style.left = x - 19 + "px"
+    } else {
+        document.getElementById("tank").style.left = "171px"
+    }
+}
