4f838462d65dc2249ffa0e503f5c73e1816b5f09
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-21 17:32:56 +0900

    xóa console.log

--- tank.js
+++ tank.js
@@ -1,63 +1,79 @@
 function moveRight() {
     let x = parseInt(document.getElementById("tank").style.left)
-    console.log(x)
-    if (x == 95) {
+    let y = parseInt(document.getElementById("tank").style.top)
+    if (x == 95 & y == 57) {
         document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("treasure").style.visibility = "hidden"
         document.getElementById("rainBow").style.visibility = "visible"
         document.getElementById("tank").style.left = x + 19 + "px"
     } else if (x < 171) {
         document.getElementById("rainBow").style.visibility = "hidden"
         document.getElementById("tank").style.visibility = "visible"
+        document.getElementById("treasure").style.visibility = "visible"
         document.getElementById("tank").style.left = x + 19 + "px"
+        document.getElementById("tank").style.top = y + "px"
     } else {
         document.getElementById("tank").style.left = "0px"
+        document.getElementById("tank").style.top = y + "px"
     }
 }
 
 function moveLeft() {
     let x = parseInt(document.getElementById("tank").style.left)
-    console.log(x)
-    if (x == 95) {
+    let y = parseInt(document.getElementById("tank").style.top)
+    if (x == 95 & y == 57) {
         document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("treasure").style.visibility = "hidden"
         document.getElementById("rainBow").style.visibility = "visible"
         document.getElementById("tank").style.left = x - 19 + "px"
     } else if (x > 0) {
         document.getElementById("rainBow").style.visibility = "hidden"
         document.getElementById("tank").style.visibility = "visible"
+        document.getElementById("treasure").style.visibility = "visible"
         document.getElementById("tank").style.left = x - 19 + "px"
+        document.getElementById("tank").style.top = y + "px"
     } else {
         document.getElementById("tank").style.left = "171px"
+        document.getElementById("tank").style.top = y + "px"
     }
 }
 
 function moveUp() {
+    let x = parseInt(document.getElementById("tank").style.left)
     let y = parseInt(document.getElementById("tank").style.top)
-    console.log(y)
-    if (y == 95) {
+    if (x == 95 & y == 57) {
         document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("treasure").style.visibility = "hidden"
         document.getElementById("rainBow").style.visibility = "visible"
-        document.getElementById("tank").style.top = y - 19 + "px"
+        document.getElementById("tank").style.top = y + 19 + "px"
     } else if (y > 0) {
         document.getElementById("rainBow").style.visibility = "hidden"
         document.getElementById("tank").style.visibility = "visible"
+        document.getElementById("treasure").style.visibility = "visible"
         document.getElementById("tank").style.top = y - 19 + "px"
+        document.getElementById("tank").style.left = x + "px"
     } else {
         document.getElementById("tank").style.top = "171px"
+        document.getElementById("tank").style.left = x + "px"
     }
 }
 
 function moveDown() {
+    let x = parseInt(document.getElementById("tank").style.left)
     let y = parseInt(document.getElementById("tank").style.top)
-    console.log(y)
-    if (y == 95) {
+    if (x == 95 & y == 57) {
         document.getElementById("tank").style.visibility = "hidden" 
+        document.getElementById("treasure").style.visibility = "hidden"
         document.getElementById("rainBow").style.visibility = "visible"
         document.getElementById("tank").style.top = y + 19 + "px"
     } else if (y < 171) {
         document.getElementById("rainBow").style.visibility = "hidden"
         document.getElementById("tank").style.visibility = "visible"
+        document.getElementById("treasure").style.visibility = "visible"
         document.getElementById("tank").style.top = y + 19 + "px"
+        document.getElementById("tank").style.left = x + "px"
     } else {
         document.getElementById("tank").style.top = "0px"
+        document.getElementById("tank").style.left = x + "px"
     }
 }
